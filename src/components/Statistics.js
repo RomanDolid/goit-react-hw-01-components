366c2617b7c97b4036e64b565bbe1c00942656cb
RomanDolid <dolid.rv@gmail.com>
2021-06-21 16:00:21 +0300

    upd settings

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -4,23 +4,23 @@ import getRandomColor from './colorPicker';
 import s from '../styles/Statistics.module.css';
 
 const Statistics = ({ stats, title }) => {
-    return (
-        <section className={s.statistics}>
-            {title && <h2 className={s.title}>{title}</h2>}
-            <ul className={s.statList}>
-                {stats.map(({ id, label, percentage }) => (
-                    <li
-                        className={s.item}
-                        key={id}
-                        style={{ backgroundColor: getRandomColor() }}
-                    >
-                        <span className={s.label}>{label}</span>
-                        <span className={s.percentage}>{percentage}%</span>
-                    </li>
-                ))}
-            </ul>
-        </section>
-    );
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+      <ul className={s.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={s.item}
+            key={id}
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 Statistics.propTypes = {
@@ -33,6 +33,5 @@ Statistics.propTypes = {
     }),
   ).isRequired,
 };
-
 
 export default Statistics;
